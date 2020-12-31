0be4a5b6166ae627cf6a0bd961521a3501cb5e63
kamil-dev <kamil.przylepa@gmail.com>
2020-12-31 12:55:51 +0100

    31.12.20

---
+++ sitka_highs.py
@@ -0,0 +1,9 @@
+import csv
+
+filename = 'sitka_weather_07-2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    print(header_row)
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
