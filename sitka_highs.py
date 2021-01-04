b3dc6c31c09d91de5be920a6c8b45d1e98113f11
kamil-dev <kamil.przylepa@gmail.com>
2021-01-04 13:55:21 +0100

    Wykres najwyzszych temperatur w Sitka(Alaska)

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,9 +1,20 @@
 import csv
-
+import matplotlib.pyplot as plt
 filename = 'sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+    print(highs)
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+ax.set_title("Najwyzsza temperatura dnia, lipiec 2018", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperatura (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+plt.show()
