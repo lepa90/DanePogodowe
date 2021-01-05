1994ef2727350ce9043c410a20346f5ed8792726
kamil-dev <kamil.przylepa@gmail.com>
2021-01-05 15:03:52 +0100

    moduł datetime

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,20 +1,24 @@
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 filename = 'sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
         high = int(row[5])
         highs.append(high)
+        dates.append(current_date)
     print(highs)
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
 ax.set_title("Najwyzsza temperatura dnia, lipiec 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperatura (F)", fontsize=16)
 ax.tick_params(axis='both', which='major', labelsize=16)
 plt.show()
